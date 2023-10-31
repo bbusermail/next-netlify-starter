5de06c0562b81a0d827048a4880d4c0149fa4774
bbusermail <30674008+bbusermail@users.noreply.github.com>
2023-11-01 01:41:06 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>PoC</title>
+        <title>Subdomain Takeover PoC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Subdomain Takeover PoC" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          by sumitsahoo.com
         </p>
       </main>
 
